9f6bb7551a7fdd7f66ce5da71544b4ce13d792b2
Anton Orlichenko <aorliche@gmail.com>
2023-06-23 22:41:48 -0500

    Got most rules working, random game and recording works

--- durak/src/durak.rs
+++ durak/src/durak.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Verb {
     Play = 1,
@@ -12,7 +15,7 @@ pub enum Verb {
 }
 
 type Card = usize;
-const unknown_card : Card = 36;
+const UNK_CARD : Card = 36;
 
 pub fn new_card(card: usize) -> Card {
     card
@@ -59,12 +62,41 @@ pub fn card_to_string(card: Card) -> String {
     format!("{} of {}", card_rank_string(card), card_suit_string(card))
 }
 
+pub fn beats(card1: Card, card2: Card, trump: Card) -> bool {
+    if card_suit(card1) == card_suit(trump) && card_suit(card1) != card_suit(card2) {
+        return true
+    }
+    card_rank(card1) > card_rank(card2) && card_suit(card1) == card_suit(card2)
+}
+    
+pub fn generate_deck() -> Vec<Card> {
+    let mut res = Vec::new();
+    for suit in 0..4 {
+        for rank in 0..9 {
+            res.push(card_from_suit_rank(suit, rank));
+        }
+    }
+    res.shuffle(&mut thread_rng());
+    res
+}
+
+pub fn remove_card(cards: &mut Vec<Card>, card: Card) -> bool {
+    let idx = cards.iter().position(|x| *x == card);
+    match idx {
+        Some(i) => {
+            cards.remove(i);
+            true
+        },
+        None => false,
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Action {
-    player: usize,
-    verb: Verb,
-    card: Card,
-    covering: Card,
+    pub player: usize,
+    pub verb: Verb,
+    pub card: Card,
+    pub covering: Card,
 }
 
 impl Action {
@@ -80,20 +112,19 @@ impl Action {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GameState {
-    defender: usize,
-    picking_up: bool,
-    trump: Card,
-    deck_size: usize,
-    plays: Vec<Card>,
-    covers: Vec<Card>,
-    hands: Vec<Vec<Card>>,
-    defering: bool,
+    pub defender: usize,
+    pub picking_up: bool,
+    pub trump: Card,
+    pub plays: Vec<Card>,
+    pub covers: Vec<Card>,
+    pub hands: Vec<Vec<Card>>,
+    pub defering: bool,
     #[serde(skip)]
-    start: Option<time::Instant>,
+    pub start: Option<time::Instant>,
 }
 
 pub fn num_not_nil(v: &Vec<Card>) -> usize {
-    v.iter().filter(|&x| *x != unknown_card).count()
+    v.iter().filter(|&x| *x != UNK_CARD).count()
 }
 
 impl GameState {
@@ -102,7 +133,6 @@ impl GameState {
             defender: 1,
             picking_up: false,
             trump,
-            deck_size: 24,
             plays: Vec::new(),
             covers: Vec::new(),
             hands,
@@ -110,41 +140,213 @@ impl GameState {
             start: None,
         } 
     }
-
     pub fn attacker_actions(&self, pidx: usize) -> Vec<Action> {
         let mut res = Vec::new();
-        if self.defering {
-            return res
-        }
         if self.plays.len() == 0 {
            for &card in &self.hands[pidx] {
-               res.push(Action::new(pidx, Verb::Play, card, unknown_card))
+               res.push(Action::new(pidx, Verb::Play, card, UNK_CARD))
            } 
         } else {
             for &card in &self.hands[pidx] {
                 for &board_card in self.plays.iter().chain(self.covers.iter()) {
-                    if board_card == unknown_card {
+                    if board_card == UNK_CARD {
                         continue;
                     }
                     // Card equality with unknown card for AI search
-                    if card_rank(card) == card_rank(board_card) || card == unknown_card {
-                        res.push(Action::new(pidx, Verb::Play, card, unknown_card));
+                    if card_rank(card) == card_rank(board_card) || card == UNK_CARD {
+                        res.push(Action::new(pidx, Verb::Play, card, UNK_CARD));
                         break;
                     }
                 }
             }
         }
         if self.picking_up || (num_not_nil(&self.covers) == self.plays.len() && self.plays.len() > 0) {
-            res.push(Action::new(pidx, Verb::Pass, unknown_card, unknown_card))
+            res.push(Action::new(pidx, Verb::Pass, UNK_CARD, UNK_CARD))
         }
         // For AI to not throw trumps away
-        if self.plays.len() > num_not_nil(&self.covers) {
-            res.push(Action::new(pidx, Verb::Defer, unknown_card, unknown_card)) 
+        if !self.picking_up && self.plays.len() > num_not_nil(&self.covers) {
+            res.push(Action::new(pidx, Verb::Defer, UNK_CARD, UNK_CARD)) 
         }
         res
     }
+    pub fn reverse_rank(&self) -> Option<usize> {
+        if self.plays.len() == 0 && self.covers.len() == 0 {
+            return None
+        }
+        let mut card = self.plays[0];
+        for &c in self.plays.iter().chain(self.covers.iter()) {
+            if c != UNK_CARD && c != card {
+                return None
+            }
+        }
+        return Some(card_rank(card))
+    }
+    pub fn defender_actions(&self, pidx: usize) -> Vec<Action> {
+        let mut res = Vec::new();
+        if self.picking_up {
+            return res
+        }
+        let revRank = self.reverse_rank();
+        match revRank {
+            Some(rank) => {
+                for &card in &self.hands[pidx] {
+                    if card_rank(card) == rank {
+                        res.push(Action::new(pidx, Verb::Reverse, card, UNK_CARD))
+                    }
+                }
+            },
+            None => (),
+        }
+        for i in 0..self.plays.len() {
+            for &card in self.hands[pidx].iter() {
+                if beats(card, self.plays[i], self.trump) {
+                    res.push(Action::new(pidx, Verb::Cover, card, self.plays[i]))
+                }
+            }
+        }
+        if self.plays.len() > 0 && self.covers.iter().filter(|&x| *x != UNK_CARD).count() < self.plays.len() {
+            res.push(Action::new(pidx, Verb::PickUp, UNK_CARD, UNK_CARD))
+        }
+        res
+    }
+    pub fn player_actions(&self, pidx: usize) -> Vec<Action> {
+        if pidx == self.defender {
+            self.defender_actions(pidx)
+        } else {
+            self.attacker_actions(pidx)
+        }
+    }
+    pub fn take_action(&mut self, action: &Action) {
+        println!("Take Action {:?}", action);
+        match action.verb {
+            Verb::Play => {
+                self.plays.push(action.card);
+                self.covers.push(UNK_CARD);
+                remove_card(&mut self.hands[action.player], action.card);
+            }, 
+            Verb::Cover => {
+                for i in 0..self.plays.len() {
+                    if action.covering == self.plays[i] {
+                        self.covers[i] = action.card;
+                    }
+                }
+                remove_card(&mut self.hands[action.player], action.card);
+            },
+            Verb::Reverse => {
+                self.plays.push(action.card);
+                self.covers.push(UNK_CARD);
+                remove_card(&mut self.hands[action.player], action.card);
+                self.defender = 1-self.defender;
+                self.defering = false;
+            },
+            Verb::PickUp => {
+                println!("PickUp");
+                self.picking_up = true;
+            },
+            Verb::Pass => {
+                if self.picking_up {
+                    for &card in self.plays.iter().chain(self.covers.iter()) {
+                        if card != UNK_CARD {
+                            self.hands[self.defender].push(card);
+                        } 
+                    } 
+                }
+                self.plays.clear();
+                self.covers.clear();
+                self.picking_up = false;
+                self.defering = false;
+            },
+            Verb::Defer => {
+                self.defering = true;
+            },
+            _ => (),
+        }
+    }
 }
 
-/*pub struct Game {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Memory {
+    pub hands: Vec<Vec<Card>>,
+    pub sizes: Vec<usize>,
+    pub discard: Vec<Card>,
+}
 
-}*/
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Game {
+    pub key: usize,
+    pub state: GameState,
+    pub deck: Vec<Card>,
+    pub memory: Memory,
+    pub recording: Vec<String>,
+    pub versus: String,
+    pub winner: Option<usize>,
+    pub joined: bool,
+}
+
+impl Game {
+    pub fn new(key: usize, versus: String) -> Game {
+        let deck = generate_deck();
+        Game {
+            key,
+            state: GameState::new(deck[0], vec![deck[30..36].to_vec(), deck[24..30].to_vec()]),
+            deck: deck[..24].to_vec(),
+            memory: Memory {
+                hands: vec![Vec::new(), Vec::new()],
+                sizes: vec![6, 6],
+                discard: Vec::new(),
+            },
+            recording: Vec::new(),
+            versus,
+            winner: None,
+            joined: false,
+        }
+    }
+    pub fn take_action(&mut self, action: &Action) {
+        self.recording.push(serde_json::to_string(&action).unwrap());
+        match action.verb {
+            Verb::Play | Verb::Cover => {
+                self.state.take_action(&action);
+                remove_card(&mut self.memory.hands[action.player], action.card);
+                self.memory.sizes[action.player] -= 1;
+            },
+            Verb::Reverse => {
+                self.state.take_action(&action);
+                remove_card(&mut self.memory.hands[action.player], action.card);
+                self.memory.sizes[action.player] -= 1;
+            },
+            Verb::Pass => {
+                if self.state.picking_up {
+                    for &card in self.state.plays.iter().chain(self.state.covers.iter()) {
+                        if card != UNK_CARD {
+                            self.memory.hands[self.state.defender].push(card);
+                            self.memory.sizes[self.state.defender] += 1;
+                        }
+                    }
+                } else {
+                    for &card in self.state.plays.iter().chain(self.state.covers.iter()) {
+                        assert!(card != UNK_CARD);
+                        self.memory.discard.push(card);
+                    }
+                }
+                self.state.take_action(&action);
+                self.deal(1-self.state.defender);
+                self.deal(self.state.defender);
+            },
+            Verb::PickUp | Verb::Defer => {
+                self.state.take_action(&action);
+            },
+        }
+    }
+    pub fn deal(&mut self, player: usize) {
+        while self.deck.len() > 0 {
+            self.state.hands[player].push(self.deck.pop().unwrap());
+        }
+        self.memory.sizes[player] = self.state.hands[player].len();
+    }
+    pub fn is_over(&self) -> bool {
+        if self.deck.len() == 0 && self.state.hands.iter().any(|x| x.len() == 0) {
+            return true
+        }
+        false
+    }
+}
--- durak/src/main.rs
+++ durak/src/main.rs
@@ -1,7 +1,27 @@
 mod durak;
 
+fn random_game() {
+    use rand::{thread_rng, seq::IteratorRandom};
+    let mut rng = thread_rng();
+
+    let mut n = 0;
+    let mut g = durak::Game::new(0, "computer".to_string());
+    while !g.is_over() {
+        n += 1;
+        if n > 100 {
+            break;
+        }
+        let p0a = g.state.player_actions(0);
+        let p1a = g.state.player_actions(1);
+        let a = p0a.iter().chain(p1a.iter()).choose(&mut rng).unwrap();
+        g.take_action(&a);
+        println!("{}", serde_json::to_string(&g).unwrap());
+    }
+
+}
+
 fn main() {
-    let c = durak::card_from_suit_rank(1,2);
+    /*let c = durak::card_from_suit_rank(1,2);
     println!("Hello, world!");
     println!("{}", durak::card_to_string(c));
     let a = durak::Action::new(0, durak::Verb::PickUp, 36, 36);
@@ -11,4 +31,24 @@ fn main() {
     let gs = durak::GameState::new(c, h);
     j = serde_json::to_string(&gs).unwrap();
     println!("{}", j);
+    let d = durak::generate_deck();
+    for card in d {
+        println!("{}", durak::card_to_string(card));
+    }
+    let mut g = durak::Game::new(0, "computer".to_string());
+    j = serde_json::to_string(&g).unwrap();
+    println!("{}", j);
+    let mut acts = g.state.attacker_actions(0);
+    for act in &acts {
+        println!("{}", serde_json::to_string(&act).unwrap());
+    }
+    g.take_action(&acts[0]);
+    println!("{}", serde_json::to_string(&g).unwrap());
+    acts = g.state.defender_actions(1);
+    for act in &acts {
+        println!("{}", serde_json::to_string(&act).unwrap());
+    }
+    g.take_action(&acts[0]);
+    println!("{}", serde_json::to_string(&g).unwrap());*/
+    random_game();
 }
